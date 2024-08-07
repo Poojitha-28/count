942ba6e8afacc86a65c6ce3db528dce01da50375
Poojitha28 <poojithavudhayagiri28@gmail.com>
2024-08-07 22:20:01 +0530

    Added counter

---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function Counter()
+{
+    const [count,SetCount]= useState(0);
+    return(
+        <div>
+            <h1>Counter App</h1> 
+            <p>Count:{count}</p>
+            <button onClick={()=>{
+                SetCount((prev)=>prev+1);
+            }}>Increment</button>
+            <button onClick={()=>{
+                SetCount((prev)=>prev-1);
+            }}>Decrement</button>
+        </div>
+    )
+}
